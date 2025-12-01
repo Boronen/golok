28643bd8dd23f23ddb7023a03bf079f1942f02e6
bmilo327 <bernathmilan7924@gmail.com>
2025-12-01 13:08:32 +0100

    megolom magam

---
+++ src/Random/java/util.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Random.java;
+
+/**
+ *
+ * @author BernáthMilán(SZF_N_2
+ */
+public class util {
+    
+}
